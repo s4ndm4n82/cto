5c6ed94a47cee39aa9ac3bc18a3b1093d9e4a9f8
Sandaruwan Samaraweera <sadaruwan12@gmail.com>
2025-01-29 14:51:01 +0530

    Fixed the json error.

--- cto/Versioning/AssemblyInfo.cs
+++ cto/Versioning/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 [assembly: AssemblyTitle("CTO")]
 
 // Version information for an assembly consists of the following four values:
-[assembly: AssemblyVersion("1.0.0.399")]
-[assembly: AssemblyFileVersion("1.0.0.399")]
-[assembly: AssemblyInformationalVersion("1.0.0.399")]
+[assembly: AssemblyVersion("1.0.0.400")]
+[assembly: AssemblyFileVersion("1.0.0.400")]
+[assembly: AssemblyInformationalVersion("1.0.0.400")]
